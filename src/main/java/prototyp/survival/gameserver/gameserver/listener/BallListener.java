937f11eaa45d082b5dde346a9b4bc037942c1479
Alexander Brand <Alexander.Brand@unitybox.de>
2022-09-08 14:40:07 +0200

    Ignore Cancelled entfernt

--- src/main/java/prototyp/survival/gameserver/gameserver/listener/BallListener.java
+++ src/main/java/prototyp/survival/gameserver/gameserver/listener/BallListener.java
@@ -25,7 +25,7 @@ public class BallListener implements Listener {
 
     private GameServer gameServer;
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         ItemStack item = event.getItem();
         if (item == null) return;
